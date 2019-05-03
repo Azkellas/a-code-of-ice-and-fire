b39faba3833ceb6f3ee59eb244b8d2952b968b16
Azke <e.desbois@hotmail.fr>
2019-05-03 21:06:07 +0200

    Nerf bosses

--- config/level1/Boss.py
+++ config/level1/Boss.py
@@ -1,12 +1,9 @@
 import sys
-def debug(*args, **kwargs):
-    print(*args, **kwargs, file=sys.stderr)
-
 import random
-random.seed()
+# make boss determinist
+random.seed(0)
 
 HQ = 0
-
 ME = 0
 
 VOID = "#"
@@ -20,71 +17,57 @@ OPPONENT_INACTIVE_CELL = "x"
 COST_UNIT = 10
 WIDTH = HEIGHT = 12
 
-class Unit:
-    def __init__(self, _id, owner, x, y):
-        self.x = x
-        self.y = y
-        self.owner = owner
-        self.id = _id
-
-
 width = int(input())
 height = int(input())
 
 def clamp_coordinates(x):
-    return min(WIDTH-1, max(0, x))
-
+	return min(WIDTH-1, max(0, x))
 
 nb_mines = int(input())
-
 for i in range(nb_mines):
-    x, y = [int(j) for j in input().split()]
+	x, y = [int(j) for j in input().split()]
 
 # game loop
 while True:
-    gold = int(input())
-    income = int(input())
+	gold = int(input())
+	income = int(input())
 
-    opponent_gold = int(input())
-    opponent_income = int(input())
+	opponent_gold = int(input())
+	opponent_income = int(input())
 
-    my_hq_pos = None
-    en_hq_pos = None
+	my_hq_pos = None
+	en_hq_pos = None
 
-    game_map = []
-    for i in range(height):
-        line = list(input())
-        game_map.append(line)
+	for i in range(height):
+		line = list(input())
 
-    building_count = int(input())
-    for i in range(building_count):
-        owner, buildingType, x, y = [int(j) for j in input().split()]
+	building_count = int(input())
+	for i in range(building_count):
+		owner, buildingType, x, y = [int(j) for j in input().split()]
 
-        if buildingType == HQ and owner == ME:
-            my_hq_pos = (x,y)
-        if buildingType == HQ and owner != ME:
-            en_hq_pos = (x,y)
+		if buildingType == HQ and owner == ME:
+			my_hq_pos = (x,y)
+		if buildingType == HQ and owner != ME:
+			en_hq_pos = (x,y)
 
-    actions = ["WAIT"]
+	actions = ["WAIT"]
 
-    my_units = []
-    unit_count = int(input())
-    for i in range(unit_count):
-        owner, unitId, level, x, y = [int(j) for j in input().split()]
-        unit = Unit(unitId, owner, x, y)
+	my_units_count = 0
+	unit_count = int(input())
+	for i in range(unit_count):
+		owner, unitId, level, x, y = [int(j) for j in input().split()]
 
-        if owner == ME:
-            my_units.append(unit)
-            actions.append("MOVE {} {} {}".format(unitId, clamp_coordinates(x+random.choice([1, -1])), clamp_coordinates(y+random.choice([1, -1]))))
+		if owner == ME:
+			my_units_count += 1
+			actions.append("MOVE {} {} {}".format(unitId, clamp_coordinates(x+random.choice([1, -1])), clamp_coordinates(y+random.choice([1, -1]))))
 
-    if gold >= COST_UNIT:
-        if my_hq_pos[0] == 0:
-            spawn_point_x = 1
-            spawn_point_y = 0
-        else:
-            spawn_point_x = my_hq_pos[0]-1
-            spawn_point_y = my_hq_pos[1]
-        actions.append("TRAIN 1 {} {}".format(spawn_point_x, spawn_point_y))
-
-    debug(actions)
-    print(";".join(actions))
+	# only one unit for wood 3 boss
+	if my_units_count == 0 and gold >= COST_UNIT:
+		if my_hq_pos[0] == 0:
+			spawn_point_x = 1
+			spawn_point_y = 0
+		else:
+			spawn_point_x = my_hq_pos[0]-1
+			spawn_point_y = my_hq_pos[1]
+		actions.append("TRAIN 1 {} {}".format(spawn_point_x, spawn_point_y))
+	print(";".join(actions))
