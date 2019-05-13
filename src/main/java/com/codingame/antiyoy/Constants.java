f948fc6dcd330d054b33c54ec0fc260e23d8b90e
Julien <julien.poulton@codingame.com>
2019-05-13 16:31:13 +0200

    Only recognise BUILD MINE and BUILD TOWER

--- src/main/java/com/codingame/antiyoy/Constants.java
+++ src/main/java/com/codingame/antiyoy/Constants.java
@@ -38,7 +38,7 @@ public final class Constants {
     public static final Pattern MOVETRAIN_PATTERN = Pattern.compile("^(MOVE|TRAIN) ([0-9]*) ([0-9]*) ([0-9]*)$");
     public static final Pattern MOVE_PATTERN = Pattern.compile("^MOVE ([0-9]*) ([0-9]*) ([0-9]*)$");
     public static final Pattern TRAIN_PATTERN = Pattern.compile("^TRAIN ([0-9]*) ([0-9]*) ([0-9]*)$");
-    public  static final Pattern BUILD_PATTERN = Pattern.compile("^BUILD ([A-Z]*) ([0-9]*) ([0-9]*)$");
+    public  static final Pattern BUILD_PATTERN = Pattern.compile("^BUILD (TOWER|MINE) ([0-9]*) ([0-9]*)$");
 
     public static final int BUILDING_COST(BUILDING_TYPE type) {
         switch (type) {
